0344ccc5489b86c77ff524b8b1abfa7b723f86d3
lane <ltdouthit@email.arizona.edu>
2018-02-26 10:40:42 -0700

    added comments and found bugs

--- src/main.m
+++ src/main.m
@@ -11,7 +11,7 @@ a = 13;
 b = 8;
 
 idx = idxVal;
-testCase = 'case14';
+testCase = 'case9';
 data = DataClass(testCase);
 %Calcultate the value of M_i
 M_i = zeros(1, data.nodes(1));
@@ -28,29 +28,31 @@ neu   = zeros(data.nodes(3),data.nodes(3));
 for i = 1:data.nodes(2)
      mi = data.network_data.branch(i,idx.FROM_BUS);
      mj = data.network_data.branch(i,idx.TO_BUS);
-     neu(mi,mj) = 0.95;
+     neu(mi,mj) = .98;
 end
-neu(13,14) = 0.5;
-%neu(1,4) = 0.15;
-omega = zeros(data.nodes(3));
+neu(1,4) = 0.8;
+neu(3,6) = 0.98;
+neu(8,2) = 0.8;
+omega = zeros(1,data.nodes(1));
+
 for i = 1:data.nodes(3)
     if ismember(i,data.gen)
-        omega(i)  = randn./100;
+        omega(i)  = 0;
     end
 end
-delta(:)  = randn(1,data.nodes(3))./100;
+delta (:) = randn(1,data.nodes(3))./10000;
 delta(1)  = 0;
 
 %
 h = 0.001; %Step Size
 
 %
-for cnt = 1:1000
+for cnt = 1:10000
     for i =1:data.nodes(1)
         react = 0;
         for j = (data.nodes(1)+1):data.nodes(3)
             delta_ij = delta(i) - delta(j);
-            react = react + data.B_ij(i,j)*sin(delta_ij);
+            react = react + data.B_ij(i,j)*sin(delta_ij); %i=2 is not getting updated
         end
         omega_dot(i) = -(D_i(i)/M_i(i))*omega(i)-(1/M_i(i))*(data.real_power(i) + react);
         omega(i) = omega_dot(i)*h + omega(i);
@@ -85,16 +87,15 @@ for cnt = 1:1000
             lamda = data.B_ij(i,j)*(1-cos(delta_ij))/data.W_ij(i,j);
             if neu(i,j) ~= 0
                 t = power_flow_f(a,b,neu(i,j));
-                neu_dot(i,j) = (t - lamda); %Bad
+                neu_dot(i,j) = (t - lamda);
             else 
                 neu_dot(i,j) = 0;
             end
             neu(i,j) = neu_dot(i,j)*h + neu(i,j);
-            if neu(i,j) > 1
-                cnt;
-                %neu(i,j) = 1.0;
-            elseif neu(i,j) < 0.01 %Bad
+            if isnan(neu(i,j)) || neu(i,j) == inf 
                 neu(i,j) = 0;
+            elseif neu(i,j) < 0.01 %Bad
+                %neu(i,j) = 0;
             end
          end
         end
@@ -102,7 +103,12 @@ for cnt = 1:1000
 
     %omega
     neu
+    omega
+    if cnt == 300
+    
+    end
+    
     cnt
-    pause(0.2)
+    pause(0.05)
   
 end
